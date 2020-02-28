991f20b1461505616bb8b16dcb071812fc5ed9d3
Nick Hackman <snickhackman@gmail.com>
2020-02-27 22:32:41 -0500

    Updated file pathes to be platform agnostic
    - also absolute paths rather than relative

--- src/game/menu/main_menu.py
+++ src/game/menu/main_menu.py
@@ -1,38 +1,45 @@
 # Setup Python ----------------------------------------------- #
 import sys
 import pygame
+import os
+import pathlib
 
 # Relative Imports
 from game.engine.game import game
 from game.menu.team_screen import team_screen
 from game.style import color as cval
-from game.style import text 
+from game.style import text
 
 
-background = pygame.image.load(r".\assets\menu\main_menu_background.png")
+PATH_TO_DIR = pathlib.Path(__file__).parent.absolute()
+
+background = pygame.image.load(
+    f"{PATH_TO_DIR}{os.sep}..{os.sep}..{os.sep}assets{os.sep}menu{os.sep}main_menu_background.png"
+)
+
 
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
- 
- 
+
+
 def main_menu(window, main_clock):
     while True:
-        window.fill(cval.black)    #fill window with black
-        window.blit(background, [0,0])
-        
-        draw_text('main menu', text.default_font, cval.black, window, 400, 200)
-     
+        window.fill(cval.black)  # fill window with black
+        window.blit(background, [0, 0])
+
+        draw_text("main menu", text.default_font, cval.black, window, 400, 200)
+
         button_1 = pygame.Rect(50, 100, 200, 50)
         button_2 = pygame.Rect(50, 200, 200, 50)
-    
+
         pygame.draw.rect(window, cval.green, button_1)
         pygame.draw.rect(window, cval.blue, button_2)
-    
+
         mx, my = pygame.mouse.get_pos()
-        
+
         clicked = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -46,7 +53,7 @@ def main_menu(window, main_clock):
                 print(event.button)
                 if event.button == 1:
                     clicked = True
-     
+
         if clicked:
             # check for click location
             if button_1.collidepoint((mx, my)):
--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 import pygame
+import os
+import pathlib
 
 # Relative Imports
 from game.menu.main_menu import main_menu
 
+PATH_TO_DIR = pathlib.Path(__file__).parent.absolute()
 
 if __name__ == "__main__":
     # Initialize pygame and the main clock
     pygame.init()
     main_clock = pygame.time.Clock()
-    
+
     # Create a window with 4:3 aspect ratio
-    window = pygame.display.set_mode((800, 600),0,32)
-    pygame.display.set_caption('HORSE')
-    window_icon = pygame.image.load(r".\assets\menu\app_icon.png")
+    window = pygame.display.set_mode((800, 600), 0, 32)
+    pygame.display.set_caption("HORSE")
+    window_icon = pygame.image.load(
+        f"{PATH_TO_DIR}{os.sep}assets{os.sep}menu{os.sep}app_icon.png"
+    )
     pygame.display.set_icon(window_icon)
 
     # Start the main menu
     main_menu(window, main_clock)
-    
