4d3ef55dfe395f3098ae039e596d805f4bcb1e47
Cameron Millspaugh <Cameron.Millspaugh@gmail.com>
2020-02-27 22:15:20 -0500

    added hacked_screen and a common folder

---
+++ src/game/menu/hacked_screen.py
@@ -0,0 +1,52 @@
+# Package imports
+import sys
+import pygame
+
+# Relative imports
+from game.style import color as cval
+from game.style import text as txt
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def hacked_screen(window, main_clock):
+    hacked_icon = pygame.image.load(r".\assets\menu\hacked_icon.png")
+    msg = txt.hacked_title.render("You've been hacked!", 1, cval.white)
+
+    running = True    
+    print("FROM HACKED_SCREEN: running =", running)
+    while running:
+        window.fill(cval.black)
+     
+        # Window height and width
+        win_width, win_height = window.get_size()
+        
+        # Place hacked_icon in top center
+        loc_x = win_width/2 - hacked_icon.get_width()/2
+        loc_y = win_height/3 - hacked_icon.get_height()/2
+        window.blit(hacked_icon, (loc_x, loc_y))
+
+        # Place hacked_icon in top center
+        loc_x = win_width/2 - msg.get_width()/2
+        loc_y = win_height*2/3 - msg.get_height()/2
+        window.blit(msg, (loc_x, loc_y))        
+        
+        # Respond to click or keyboard events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        
+        # Update window and set clock speed
+        pygame.display.update()
+        main_clock.tick(60)
+        
+    print("FROM TEAM_SCREEN: running =", running)
+
--- src/game/menu/main_menu.py
+++ src/game/menu/main_menu.py
@@ -1,58 +1,25 @@
-# Setup Python ----------------------------------------------- #
+# Package imports
 import pygame
 
 # Relative Imports
 from game.engine.game import game
+from game.menu.hacked_screen import hacked_screen
 from game.menu.team_screen import team_screen
+from common.button import Button
 from game.style import color as cval
 from game.style import text as txt
 
-def draw_text(text, font, color, surface, x, y):
-    textobj = font.render(text, 1, color)
-    textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
-    surface.blit(textobj, textrect)
-
-class Button():
-    def __init__(self, color, x, y, width, height, text=''):
-        self.color = color
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-
-    def draw(self,win,outline=None):
-        #Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
-            
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
-        
-        if self.text != '':
-            font = txt.menu_button
-            # Create text object from string
-            text = font.render(self.text, 1, (0,0,0))
-            # Overlay object onto button
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
-
-    def mouse_over(self, pos):
-        # Mouse within allowable x range
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            # Mouse within allowable y range
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-        return False
 
 def main_menu(window, main_clock, background):
-    title = "HORSE"
-    title = txt.menu_title.render(title, 1, cval.black)
+    # Set variables
+    title = txt.menu_title.render("HORSE", 1, cval.black)
     running = True
+    
     while running:
         window.fill(cval.black)    # Default to fill window with black
         window.blit(background, (0,0))  # Overlay background image
 
-        # window height and width
+        # Window height and width
         win_width, win_height = window.get_size()
         
         # Place Title in top center
@@ -75,6 +42,9 @@ def main_menu(window, main_clock, background):
         # Display menu buttons
         play_btn.draw(window)
         team_menu_btn.draw(window)
+        
+        hacked_screen_btn = Button(cval.white, 20, 20, btn_width, btn_height, "Hacked")
+        hacked_screen_btn.draw(window)
     
         # Get mouse position
         mx, my = pygame.mouse.get_pos()
@@ -98,6 +68,8 @@ def main_menu(window, main_clock, background):
                 game(window, main_clock)
             if team_menu_btn.mouse_over((mx, my)):
                 team_screen(window, main_clock)
+            if hacked_screen_btn.mouse_over((mx, my)):
+                hacked_screen(window, main_clock)
 
         pygame.display.update()
         main_clock.tick(60)
