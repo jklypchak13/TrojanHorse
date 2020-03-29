c2793affa469bbea7df2831c874cd5029f0376d7
Aidan Globus <aidanglobus@gmail.com>
2020-03-28 22:00:53 -0400

    Reorganized collisionmanager to better reflect UML plan

--- src/game/engine/collision_manager.py
+++ src/game/engine/collision_manager.py
@@ -10,21 +10,24 @@ class CollisionManager:
 
     def check_all_collisions(self):
         #Check dynamic and player against static
+        self.phys_collides_static()
+        self.player_collides_static()
+        self.player_collides_phys()
+
+
+    def phys_collides_static(self):
         for s in self.static_objects:
             for d in self.dyanmic_objects:
                 if s.contains(d):
                     s.handle_collision(d)
+
+    def player_collides_static(self):
+        for s in self.static_objects:
             if s.contains(self.player):
                 s.handle_collision(self.player)
 
+    def player_collides_phs(self):
         #Check dyanmic objects against Player
         for d in self.dyanmic_objects:
             if d.contains(self.player):
                 d.handle_collision(player)
-
-
-    def draw_all(self):
-        for o in self.game_objects:
-            o.draw(self.screen, self.screen_offset)
-        self.player.draw(self.screen, self.screen_offset)
-        pygame.display.update()
