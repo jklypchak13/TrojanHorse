6a3e1b2c9043a6f2c1f2912fbc975182e75708b1
NickHackman <snickhackman@gmail.com>
2020-04-05 18:40:34 -0400

    Flip image based on velocity
    Horsey now changes directions

--- src/game/engine/physics_object.py
+++ src/game/engine/physics_object.py
@@ -23,12 +23,19 @@ class PhysicsObject(GameObject):
         super().__init__(position, image_path)
         self.x_vel = x_vel
         self.y_vel = y_vel
+        image = pygame.image.load(image_path)
+        self.image_right: pygame.Surface = self.image
+        self.image_left: pygame.Surface = pygame.transform.flip(self.image, True, False)
         self.gravity = 0.9
 
     def update_x(self) -> None:
         """
         Update the position of this object based on its velocity x values.
         """
+        if self.x_vel < 0:
+            self.image = self.image_left
+        elif self.x_vel > 0:
+            self.image = self.image_right
         self.position.x += self.x_vel
 
     def update_y(self) -> None:
